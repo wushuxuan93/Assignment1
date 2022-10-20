817f39612a8b300bc95a56d504a70ce43e3495a3
Shuxuan Wu <wushuxuan93@gmail.com>
2022-10-20 10:39:32 -0400

    converted project to typescript

--- .history/app_20221020103551.js
+++ .history/app_20221020103551.js
@@ -10,14 +10,15 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const index_1 = __importDefault(require("./Routes/index"));
 const app = (0, express_1.default)();
-app.set('views', path_1.default.join(__dirname, 'Views'));
+app.set('views', path_1.default.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use((0, morgan_1.default)('dev'));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cookie_parser_1.default)());
-app.use(express_1.default.static(path_1.default.join(__dirname, 'Public')));
+app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
 app.use('/', index_1.default);
+app.use('/users', users_1.default);
 app.use(function (req, res, next) {
     next((0, http_errors_1.default)(404));
 });
