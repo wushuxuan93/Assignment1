22ede9f5a31f438a0d3fc83f5318ef2d42cbc7f2
Shuxuan Wu <wushuxuan93@gmail.com>
2022-10-20 13:30:06 -0400

    add basic routes

--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -6,7 +6,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Home', page: 'home' });
+});
+router.get('/home', function (req, res, next) {
+    res.render('index', { title: 'Home', page: 'home' });
+});
+router.get('/about', function (req, res, next) {
+    res.render('index', { title: 'About us', page: 'about' });
+});
+router.get('/projects', function (req, res, next) {
+    res.render('index', { title: 'Projects', page: 'projects' });
+});
+router.get('/serves', function (req, res, next) {
+    res.render('index', { title: 'Serves', page: 'serves' });
+});
+router.get('/contact', function (req, res, next) {
+    res.render('index', { title: 'Contact', page: 'contact' });
 });
 exports.default = router;
 //# sourceMappingURL=index.js.map
